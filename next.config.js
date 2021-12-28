f797b97e168d1f43fc72bb0a44ce3ccbda626228
sato <sato_m@mediaxis.jp>
2021-12-28 15:54:53 +0900

    bundle sizeを把握できるようにanalyzerを追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   eslint: {
     dirs: ["src"],
   },
-};
+});
