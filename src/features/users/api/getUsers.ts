4654613f636c3c91ffa0580e7ca00a1520400ce9
sato <sato_m@mediaxis.jp>
2021-12-29 23:49:58 +0900

    APIエラーをハンドリングできるようにした

--- src/features/users/api/getUsers.ts
+++ src/features/users/api/getUsers.ts
@@ -3,7 +3,10 @@ import useAspidaSWR from "@aspida/swr";
 import { client } from "@/lib/api";
 
 export const useUsers = () => {
-  const { data, error } = useAspidaSWR(client.users, "get");
+  const { data, error } = useAspidaSWR(client.users, "get", {
+    revalidateOnFocus: false,
+    shouldRetryOnError: false,
+  });
 
   return {
     users: data?.body.users,
--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,8 +1,19 @@
 import aspida from "@aspida/axios";
-import axios from "axios";
+import Axios from "axios";
 
 import api from "./aspida/$api";
 
-export const client = api(
-  aspida(axios, { baseURL: process.env.NEXT_PUBLIC_API_URL })
+const axios = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // TODO: エラートーストを表示する
+    console.error("Error Notification", error);
+    return Promise.reject(error);
+  }
 );
+
+export const client = api(aspida(axios));
